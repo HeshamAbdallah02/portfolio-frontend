55a9035bcee742f8be04c7dca02b3f6bdbff616a
HeshamAbdallah02 <75794796+HeshamAbdallah02@users.noreply.github.com>
2025-02-03 05:23:50 +0200

    modified:   src/config/api.config.js

--- src/config/api.config.js
+++ src/config/api.config.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = 'https://portfolio-backend-helgxq.fly.dev';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://portfolio-backend-helgxq.fly.dev';
 
 export default API_BASE_URL;
